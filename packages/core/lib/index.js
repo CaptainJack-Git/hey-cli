7d2c7ce8de2863ad278947aaa7aa4ccec62c07bf
CaptainJack <captain_jack_mail@163.com>
2022-06-04 21:53:38 +0800

    添加root账户的降级

--- packages/core/lib/index.js
+++ packages/core/lib/index.js
@@ -13,6 +13,8 @@ function core(argvs) {
   } catch (err) {
     log.error(err.message)
   }
+
+  checkRoot()
 }
 
 function checkPkgVersion() {
@@ -25,6 +27,18 @@ function checkNodeVersion() {
   if (!semver.gte(currentVersion, LOWEST_NODE_VERSION)) {
     throw new Error(colors.red(`hey-cli 需要最低的 node 版本为 ${LOWEST_NODE_VERSION}`))
   }
+}
+
+// 检查当前账户是否是root账户
+function checkRoot() {
+  // 当 process.geteuid() 为0时，说明使用的是root用户. ps:不同的平台的普通用户的pid不一样
+  log.info(process.geteuid())
+  // 以下方法可以对root用户做降级
+  // require()不支持 ES6 模块的一个原因是，它是同步加载，而 ES6 模块内部可以使用顶层await命令，导致无法被同步加载。
+  ;(async () => {
+    await import('root-check')
+    log.info(process.geteuid())
+  })()
 }
 
 module.exports = core
