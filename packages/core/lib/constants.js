cc449eb6ca60fb493381c157d47f8a0398e03e91
CaptainJack <captain_jack_mail@163.com>
2022-06-04 21:39:29 +0800

    通过semver来做版本检查

---
+++ packages/core/lib/constants.js
@@ -0,0 +1,6 @@
+// 脚手架运行的最低版本号
+const LOWEST_NODE_VERSION = '14.0.0'
+
+module.exports = {
+  LOWEST_NODE_VERSION,
+}
--- packages/core/lib/index.js
+++ packages/core/lib/index.js
@@ -1,12 +1,30 @@
+const semver = require('semver')
+const colors = require('colors')
+
 const pkg = require('../package.json')
+const { LOWEST_NODE_VERSION } = require('../lib/constants')
+const { log } = require('@hey-cli/utils')
 
 function core(argvs) {
   console.log('执行入口', argvs)
-  checkPkgVersion()
+  try {
+    checkPkgVersion()
+    checkNodeVersion()
+  } catch (err) {
+    log.error(err.message)
+  }
 }
 
 function checkPkgVersion() {
   console.log(pkg.version)
+}
+
+// 设置最低版本号，对比用户node版本是否可以运行
+function checkNodeVersion() {
+  const currentVersion = process.version
+  if (!semver.gte(currentVersion, LOWEST_NODE_VERSION)) {
+    throw new Error(colors.red(`hey-cli 需要最低的 node 版本为 ${LOWEST_NODE_VERSION}`))
+  }
 }
 
 module.exports = core
